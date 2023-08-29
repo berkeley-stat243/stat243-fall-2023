8950dec0d7098f2bc63cde3d97155d01930a2798
Christopher Paciorek <paciorek@stat.berkeley.edu>
2023-08-29 08:59:58 -0700

    debugging rmarkdown installation #2

--- install_requirements.R
+++ install_requirements.R
@@ -3,6 +3,10 @@
 requirements <- c('rmarkdown')     
 
 system("ls -l /usr/local/lib/R/site-library")
+system("ls -l /usr/local/lib/R")
+
+print('foo')
+system("ls -l /usr/lib/R/site-library")
 print(.libPaths())
 
 for (package in requirements) 
